3c5f1c2d542884a59c1330ba6e1eb62accd1734b
keitaj <keita.curry.jp@gmail.com>
2022-04-29 15:06:12 +0900

    show sol balance

--- src/components/Connect2Phantom.tsx
+++ src/components/Connect2Phantom.tsx
@@ -1,5 +1,6 @@
 import { FC, useEffect, useState } from "react";
-import { Connection, PublicKey, clusterApiUrl } from "@solana/web3.js";
+import { Connection, PublicKey, clusterApiUrl, LAMPORTS_PER_SOL, RpcResponseAndContext, AccountInfo, Commitment } from "@solana/web3.js";
+import {AccountLayout, TOKEN_PROGRAM_ID, getAccount, Account} from "@solana/spl-token";
 
 type PhantomEvent = "disconnect" | "connect" | "accountChanged";
 
@@ -24,7 +25,7 @@ const Connect2Phantom: FC = () => {
     const [ provider, setProvider ] = useState<PhantomProvider | null>(null);
     const [ connected, setConnected ] = useState(false);
     const [ pubKey, setPubKey ] = useState<PublicKey | null>(null);
-
+    const [ solBalance, setSolBalance ] = useState<number | null>(null);
 
     useEffect( ()=>{
         if ("solana" in window) {
@@ -43,6 +44,55 @@ const Connect2Phantom: FC = () => {
             console.log(`connect event: ${publicKey}`);
             setConnected(true); 
             setPubKey(publicKey);
+
+            const getSolBalance = async () : Promise<number> => {
+                return new Promise((resolve) => {
+                    try {
+                        const connection = new Connection(clusterApiUrl("devnet"),"confirmed");
+                        const balance = connection.getBalance(publicKey);
+                        resolve(balance);
+                    } catch(err) {
+                        console.log(err);
+                    }
+                });
+            }
+            getSolBalance().then((solbalance: number) => {
+                setSolBalance(solbalance/LAMPORTS_PER_SOL);
+              });
+            // const getKtaTokenAccount = async () : Promise<RpcResponseAndContext<{
+            //     pubkey: PublicKey;
+            //     account: AccountInfo<Buffer>;
+            // }[]>> => {
+            //     return new Promise((resolve) => {
+            //         try {
+            //             const connection = new Connection(clusterApiUrl("devnet"),"confirmed");
+            //             const tokenAccounts = connection.getTokenAccountsByOwner(
+            //                 // new PublicKey("Begfxys71yZxL6e1VzzSuCxeH2wEaoLZVtdwNKHqr75R"),
+            //                     publicKey,
+            //                 {
+            //                     programId: TOKEN_PROGRAM_ID,
+            //                 }
+            //               );
+            //             resolve(tokenAccounts)
+            //         } catch(err) {
+            //             console.log(err);
+            //         }
+            //     });
+            // }
+            // getKtaTokenAccount().then((tokenAccounts: RpcResponseAndContext<{
+            //     pubkey: PublicKey;
+            //     account: AccountInfo<Buffer>;
+            // }[]>) => {
+            //     console.log("Token Balance");
+            //     console.log("------------------------------------------------------------");
+            //     tokenAccounts.value.forEach((e) => {
+            //         // console.log(e.pubkey.toBase58);
+            //         const accountInfo = AccountLayout.decode(e.account.data);
+            //         // console.log(accountInfo.amount);
+            //         console.log(`${new PublicKey(accountInfo.mint)}   ${accountInfo.amount}`);
+            //     })
+            //   });
+
         });
         provider?.on("disconnect", ()=>{ 
             console.log("disconnect event");
@@ -71,7 +121,8 @@ const Connect2Phantom: FC = () => {
                 <>
                 <button disabled={connected} onClick={connectHandler}>Connect to Phantom</button>
                 <button disabled={!connected} onClick={disconnectHandler}>Disconnect from Phantom</button>
-                { connected ? <p>Your public key is : {pubKey?.toBase58()}</p> : null }
+                { connected ? <p>Your public key is : {pubKey?.toBase58()} </p> : null }
+                { connected ? <p>Your sol Balance is : {solBalance}</p> : null }
                 </>
             :
                 <>
